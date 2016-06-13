0580854795ed3e70aad6f821e1c42992c62151b8
kota koyama <kota-koyama@dac.co.jp>
2016-06-13 06:37:36 +0000

    mede loginpage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,20 @@
 class UsersController < ApplicationController
 
   def show 
-   @user = User.find(params[:id])
+    @user = User.find(params[:id])
+    render 'show'
   end
   
   def new
     @user = User.new
+    render 'new'
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user 
+      redirect_to user_path(@user) 
     else
       render 'new'
     end
